1ceb65f4c6d4540f357c532e364ba959b127b9eb
Jan Robert <janmartinrobert@gmail.com>
2021-08-16 11:01:13 +0200

    Pull PageLayout and Header up into App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import UserSearch from './pages/UserSearch'
+import PageLayout from './components/PageLayout'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={UserSearch} />
-      </Switch>
+      <PageLayout>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={UserSearch} />
+        </Switch>
+      </PageLayout>
     </Router>
   )
 }
