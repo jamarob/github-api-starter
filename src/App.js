60fa9f4135bcdfcd95085dfe987b3857cdfbe0b7
Jan Robert <janmartinrobert@gmail.com>
2021-08-16 10:33:08 +0200

    Install prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import {useEffect, useState} from "react";
-import {getLoggedInUser} from "./service/github-api";
+import './App.css'
+import { useEffect, useState } from 'react'
+import { getLoggedInUser } from './service/github-api'
 
 function App() {
-  const [profile,setProfile] = useState()
-  const [error,setError] = useState()
+  const [profile, setProfile] = useState()
+  const [error, setError] = useState()
 
   useEffect(() => {
-    getLoggedInUser().then(setProfile).catch(error => setError(error.response.status))
-  },[])
+    getLoggedInUser()
+      .then(setProfile)
+      .catch(error => setError(error.response.status))
+  }, [])
 
-  if(!profile && !error){
+  if (!profile && !error) {
     return <p>loading</p>
   }
 
   return (
     <div className="App">
-      {profile && <img src={profile.avatar_url} alt={profile.login}/>}
+      {profile && <img src={profile.avatar_url} alt={profile.login} />}
       {error && <img src={`https://http.cat/${error}`} alt={error} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/service/github-api.js
+++ src/service/github-api.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
 const token = process.env.REACT_APP_GITHUB_TOKEN
 
 const instance = axios.create({
-    timeout: 1000,
-    headers: {Authorization: `Bearer ${token}`},
+  timeout: 1000,
+  headers: { Authorization: `Bearer ${token}` },
 })
 
-
-export const getLoggedInUser = () => instance.get("https://api.github.com/user").then(response => response.data)
+export const getLoggedInUser = () =>
+  instance.get('https://api.github.com/user').then(response => response.data)
