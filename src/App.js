6282178de47e84dd3f235a5ec3f10a0c164007e8
Jan Robert <janmartinrobert@gmail.com>
2021-08-16 10:40:21 +0200

    Remove example content

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import './App.css'
-import { useEffect, useState } from 'react'
-import { getLoggedInUser } from './service/github-api'
-
 function App() {
-  const [profile, setProfile] = useState()
-  const [error, setError] = useState()
-
-  useEffect(() => {
-    getLoggedInUser()
-      .then(setProfile)
-      .catch(error => setError(error.response.status))
-  }, [])
-
-  if (!profile && !error) {
-    return <p>loading</p>
-  }
-
-  return (
-    <div className="App">
-      {profile && <img src={profile.avatar_url} alt={profile.login} />}
-      {error && <img src={`https://http.cat/${error}`} alt={error} />}
-    </div>
-  )
+  return <div>hello</div>
 }
 
 export default App
